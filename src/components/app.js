db5066e58ac1bdb8a4b043cfa19d1c03b6a4c04b
MarieJohanna <mariejohannanilsson@gmail.com>
2018-09-04 20:03:30 +0200

    hash

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter, Route } from "react-router-dom"
+import { HashRouter, Route } from "react-router-dom"
 import Home from "./home/home"
 import Resume from "./resume/resume"
 import ProjectInfo from "./work/projectInfo"
@@ -9,13 +9,13 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
+        <HashRouter>
           <div>
             <Route exact path="/" component={Home} />
             <Route path="/resume" exact component={Resume} />
             <Route path="/projects/:id" component={ProjectInfo} />
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     )
   }
