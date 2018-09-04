9d2ab97ae3522e1179a5e8cca4ce0d966a291d71
MarieJohanna <mariejohannanilsson@gmail.com>
2018-09-04 22:27:02 +0200

    another try

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,6 @@
 import React from "react"
 import { HashRouter, Route } from "react-router-dom"
 import Home from "./home/home"
-import About from "./about/about"
-import Footer from "./footer/footer"
 import Resume from "./resume/resume"
 import ProjectInfo from "./work/projectInfo"
 
@@ -13,10 +11,10 @@ class App extends React.Component {
       <div>
         <HashRouter>
           <div>
-            <Route path="/" component={Home} />
-            <Route path="/#about" component={About} />
-            <Route path="/#contact" component={Footer} />
-            <Route path="/resume" exact component={Resume} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={Home} />
+            <Route exact path="/contact" component={Home} />
+            <Route exact path="/resume" component={Resume} />
             <Route path="/projects/:id" component={ProjectInfo} />
           </div>
         </HashRouter>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,27 @@
 import React from "react"
-import { HashLink as Link } from 'react-router-hash-link'
-// import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import "./header.css"
 
 class Header extends React.Component {
+
+  scrollToLocation = () => {
+    const { hash } = window.location
+    if (hash !== "") {
+      let retries = 0
+      const id = hash.replace("#", "")
+      const scroll = () => {
+        retries += 0
+        if (retries > 50) return
+        const element = document.getElementById(id)
+        if (element) {
+          setTimeout(() => element.scrollIntoView(), 0)
+        } else {
+          setTimeout(scroll, 100)
+        }
+      }
+      scroll()
+    }
+  }
 
   render() {
     return (
@@ -11,15 +29,14 @@ class Header extends React.Component {
         <nav>
           <ul>
             <li><h4><Link to="/" className="nav-link">Home</Link></h4></li>
-            <li><h4><Link to="/#about" className="nav-link">About</Link></h4></li>
+            <li><h4><a href="/#about" className="nav-link">About</a></h4></li>
             <li><h4><Link to="/resume" className="nav-link">Resume</Link></h4></li>
-            <li><h4><Link to="/#contact" className="nav-link">Contact</Link></h4></li>
+            <li><h4><a href="/#contact" className="nav-link">Contact</a></h4></li>
           </ul>
         </nav>
       </header>
     )
   }
-
 }
 
 export default Header
